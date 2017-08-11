71f2d91295e71f3f68869bdbfb7320cc1052873d
sdnuliulei <sdnuliulei@gmail.com>
2017-08-11 11:14:01 +0800

    program update

---
+++ AppConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeneralApp.Infrastructure.Helper;
+
+namespace AppConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoggerHelper.WriteToFile("this is my firstLog");
+            Console.ReadKey();
+        }
+    }
+}
